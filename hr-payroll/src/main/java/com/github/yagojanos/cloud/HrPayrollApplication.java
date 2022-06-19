a473288ac326c30161f26673b1eb014afa668c61
Yago Janos <yjquaranta@gmail.com>
2022-06-18 22:23:58 -0300

    Project hr-payroll created

---
+++ hr-payroll/src/main/java/com/github/yagojanos/cloud/HrPayrollApplication.java
@@ -0,0 +1,12 @@
+package com.github.yagojanos.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrPayrollApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HrPayrollApplication.class, args);
+    }
+}
